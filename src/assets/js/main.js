290aaa5dc22e84114ed2339362a4cea7a0b94c95
Joshua Dowding <joshuadowding@outlook.com>
2021-11-14 18:53:04 +0000

    project: initial progress towards website redesign

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,22 @@
 $(() => {
-    
+  let nav = document.querySelector("div[class='nav-content']");
+  if (nav) {
+    let path = window.location.pathname;
+    if (path.includes("rabbitsandravens")) {
+      let nav_i = nav.querySelector("div[id='nav-i-r']");
+      if (nav_i) {
+        nav_i.classList.add("selected");
+      }
+    } else if (path.includes("blog")) {
+      let nav_i = nav.querySelector("div[id='nav-i-b']");
+      if (nav_i) {
+        nav_i.classList.add("selected");
+      }
+    } else if (path.includes("portfolio")) {
+      let nav_i = nav.querySelector("div[id='nav-i-p']");
+      if (nav_i) {
+        nav_i.classList.add("selected");
+      }
+    }
+  }
 });
